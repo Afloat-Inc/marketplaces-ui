955fb010cba9d72d7d140f1c2b82fc926ba74ecf
Jose <jmgayosso@mightywizards.com>
2022-01-28 18:10:51 -0600

    Init project added pug and styl loaders, alias webpack cofig

---
+++ src/components/testingVue3.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'testing-vue-3'
+}
+</script>
+
+<template lang="pug">
+div
+    .text-bold.test.global-font.text-primary This is a test component
+</template>
+
+<style lang="stylus">
+.test
+    // color: $negative 
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+q-page(class="flex flex-center")
+    testing-vue-3
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import TestingVue3 from '../components/testingVue3'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {
+    TestingVue3
+  }
+})
+</script>
