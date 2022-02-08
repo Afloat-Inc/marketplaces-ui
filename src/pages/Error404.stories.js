a7f267d40256b3590b2fe1352b3319ce0c629bd8
AlejandroM1ghty <alejandro@m1ghty.io>
2022-02-08 16:53:38 -0600

    Storybook added [webpack5]

---
+++ src/pages/Error404.stories.js
@@ -0,0 +1,22 @@
+import NotFound from './Error404.vue';
+
+export default {
+  title: 'Example/ Not Found',
+  component: NotFound,
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { NotFound },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    // Story args can be spread into the returned object
+    return { ...args };
+  },
+  // Then, the spread values can be accessed directly in the template
+  template: '<NotFound/>',
+});
+
+export const Base = Template.bind({});
+Base.args = {
+};
