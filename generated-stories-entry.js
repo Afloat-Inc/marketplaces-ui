d7de7f44990186731e0ccaef008879f7a7b723d4
AlejandroM1ghty <alejandro@m1ghty.io>
2022-02-10 17:20:21 -0600

    Files copied from DAO LLC

---
+++ generated-stories-entry.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var _frameworkImportPath = require('@storybook/vue');
+
+(0, _frameworkImportPath.configure)([require.context('./src', true, /^\.(?:(?:^|\/|(?:(?:(?!(?:^|\/)\.).)*?)\/)(?!\.)(?=.)[^/]*?\.stories\.mdx)$/), require.context('./src', true, /^\.(?:(?:^|\/|(?:(?:(?!(?:^|\/)\.).)*?)\/)(?!\.)(?=.)[^/]*?\.stories\.(js|jsx|ts|tsx))$/)], module, false)
