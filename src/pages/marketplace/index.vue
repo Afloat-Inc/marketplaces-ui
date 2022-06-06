718bbe952e35beb5414334d0a46a34050a7dc38f
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-06 15:26:05 -0500

    Modified the index of marketplace

--- src/pages/marketplace/index.vue
+++ src/pages/marketplace/index.vue
@@ -3,6 +3,9 @@
   .text-h6 Marketplace
   Tinput(label="lorem")
   IpfsMultiInput(v-model="files")
+  div(v-for="market in markets" @click="selectMarket(market)")
+    .text-h6
+      | {{market.name}}
 </template>
 
 <script>
@@ -17,7 +20,32 @@ export default {
   data () {
     return {
       keyToEncrypt: '',
-      files: undefined
+      files: undefined,
+      markets: [
+        {
+          id: 1,
+          name: 'market1',
+          price: '0.1',
+          description: 'lorem ipsum'
+        },
+        {
+          id: 2,
+          name: 'market2',
+          price: '0.2',
+          description: 'lorem ipsum'
+        },
+        {
+          id: 3,
+          name: 'market3',
+          price: '0.3',
+          description: 'lorem ipsum'
+        }
+      ]
+    }
+  },
+  methods: {
+    selectMarket (market) {
+      this.$router.push({ name: 'marketplace-detail', params: { id: market.id } })
     }
   }
 }
