3e3e7d16ee0e96a36eb90cda18b4f8a564f33fcb
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-06 17:22:23 -0500

    Created component to show the information of the marketplace

--- src/components/marketplace/details/market-info.vue
+++ src/components/marketplace/details/market-info.vue
@@ -3,23 +3,26 @@
   q-card(bordered flat)
     q-card-section
       .row.justify-center
-        .text-h6 {{market.name}}
+        .text-h6 {{market.label}}
     q-card-section
       .row
         .col-6
           .text-subtitle2 Owner
-          account-item(:address="market.owner" flat)
+          account-item(:address="market.administrator" flat)
         .col-6
           .row.justify-end
             .text-subtitle2 State of Tax Credits
           .row.justify-end
             .text-subtitle2 Michigan
     q-card-section
+      .row.justify-center.text-subtitle2 Tax Credits
+      q-separator
       .row
-        .col-6
-          .text-subtitle2 Tax Credits
-          .text-subtitle2 {{market.taxCredits}}
-    q-separator
+        .col-12
+          .text-subtitle2(v-for="credit in marketInfo.taxCredits")
+            .row.justify-between
+              div {{credit.state}}
+              div {{credit.amount}}
     q-card-section
 </template>
 
@@ -38,7 +41,23 @@ export default {
     return {
       marketInfo: {
         owner: '',
-        taxCredits: ''
+        taxCredits: [
+          {
+            state: 'Michigan',
+            taxCredits: 'lorem',
+            amount: '500.30'
+          },
+          {
+            state: 'Ohio',
+            taxCredits: 'lorem',
+            amount: '800.30'
+          },
+          {
+            state: 'Pennsylvania',
+            taxCredits: 'lorem',
+            amount: '1000.30'
+          }
+        ]
       }
     }
   }
