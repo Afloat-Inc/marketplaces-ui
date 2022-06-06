6fb0d94894bcceea41d4ee23f92c387762710131
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-06 15:25:14 -0500

    Added component to show marketplace information

---
+++ src/components/marketplace/details/market-info.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+#container
+  q-card(bordered flat)
+    q-card-section
+      .row.justify-center
+        .text-h6 {{market.name}}
+    q-card-section
+      .row
+        .col-6
+          .text-subtitle2 Owner
+          account-item(:address="market.owner" flat)
+        .col-6
+          .row.justify-end
+            .text-subtitle2 State of Tax Credits
+          .row.justify-end
+            .text-subtitle2 Michigan
+    q-card-section
+      .row
+        .col-6
+          .text-subtitle2 Tax Credits
+          .text-subtitle2 {{market.taxCredits}}
+    q-separator
+    q-card-section
+</template>
+
+<script>
+import AccountItem from '~/components/common/account-item.vue'
+export default {
+  name: 'MarketInfo',
+  components: { AccountItem },
+  props: {
+    market: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      marketInfo: {
+        owner: '',
+        taxCredits: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
