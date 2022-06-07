385711a26680ffe4de57441243fa82245a86642e
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-07 13:39:52 -0500

    Added new constant files

---
+++ src/services/const/allMarkets.js
@@ -0,0 +1,23 @@
+const markets = [
+  {
+    id: '0',
+    label: "Chema's marketplace",
+    administrator: '5CmFmVadzNQFaeiyXXNugRXT1MuaoocUyogtYHEQeWjGp7pX'
+  },
+  {
+    id: '1',
+    label: "Abel's marketplace",
+    administrator: '5DaWmLfzBTLbKFwBC5YxtAQ45XMSAQCDLcZL6zW9ZiJsGSST'
+  },
+  {
+    id: '2',
+    label: "Erick's marketplace",
+    administrator: '5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym'
+  },
+  {
+    id: '3',
+    label: "Alejandro's marketplace",
+    administrator: '5GEEZx22MqCvBDKtFPgTiKAVkoHp1vMTsw8e7fjtRH8Ldzsu'
+  }
+]
+export default markets
---
+++ src/services/const/participants.js
@@ -0,0 +1,12 @@
+const participants = [
+  {
+    address: '5DU84E1JYAhftyimxYd1MUaQ82GBKxNVFhDJSUSGU1ULpg1C'
+  },
+  {
+    address: '5CZo4UL6tCS7jsXybRV1kzRZV9uKLy2yzCfQBmbQLx1x4Nce'
+  },
+  {
+    address: '5GEEZx22MqCvBDKtFPgTiKAVkoHp1vMTsw8e7fjtRH8Ldzsu'
+  }
+]
+export default participants
