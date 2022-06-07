6035d23de452d0911016099aaa6f75c887790cf4
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-07 13:38:56 -0500

    Registered the new polkadot class [pallet]

--- src/boot/api.js
+++ src/boot/api.js
@@ -1,12 +1,14 @@
 /* eslint-disable dot-notation */
 import PolkadotApi from '~/services/polkadotApi.js'
-import { NbvStorageApi } from '~/services/polkadot-pallets'
+import { NbvStorageApi, MarketplaceApi } from '~/services/polkadot-pallets'
 
 export default async ({ app, store }) => {
   const api = new PolkadotApi()
   await api.connect()
   console.log('PolkadotApiCreated', api)
   const nbvStorageApi = new NbvStorageApi(api)
+  const marketplaceApi = new MarketplaceApi(api)
   store['$polkadotApi'] = api
   store['$nbvStorageApi'] = nbvStorageApi
+  store['$marketplaceApi'] = marketplaceApi
 }
