ca764dd07eac06aff31dc27329a0f7e46e738f1e
Jose <josemaria@hashed.io>
2022-06-07 13:56:46 -0500

    Implementing jest to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,9 @@ module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-actions"],
   "framework": "@storybook/vue3",
+  features: {
+    interactionsDebugger: true, // 👈 enable playback controls
+  },
   core: {
     builder: "webpack5"
   },
--- src/stories/components/marketplace/create-marketplace-form.stories.js
+++ src/stories/components/marketplace/create-marketplace-form.stories.js
@@ -22,30 +22,66 @@ const Template = (args) => ({
   setup () {
     return { args }
   },
+  data () {
+    return {
+      data: undefined
+    }
+  },
   // And then the `args` are bound to your component with `v-bind="args"`
   template: '<CreateMarketplaceForm v-bind="args" @submittedForm="submitAction"/>',
   methods: {
     submitAction (e) {
       action('submittedForm').call(null, e)
+      this.data = e
+      console.log('data', this.data)
     }
   }
 })
 
+// Base component
 export const Base = Template.bind({})
-Base.play = async ({ args, canvasElement }) => {
+
+// Success Submitted
+export const SuccessSubmitted = Template.bind({
+  data () {
+    return {
+      data: 'Testing'
+    }
+  }
+})
+SuccessSubmitted.play = async ({ args, canvasElement }) => {
   const canvas = within(canvasElement)
 
   await userEvent.type(canvas.getByTestId('label_input'), 'This is a description')
 
   await userEvent.click(canvas.getByTestId('submit_btn'))
-  console.log('args', args)
+  // console.log('args', args)
+  // const spy = spyOn(CreateMarketplaceForm, 'submittedForm')
+
+  // await waitFor(() => expect(args).toHaveBeenCalled())
+}
+
+// Failed Submitted
+export const FailedSubmitted = Template.bind({})
+FailedSubmitted.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+
+  await userEvent.type(canvas.getByTestId('label_input'), undefined)
+
+  await userEvent.click(canvas.getByTestId('submit_btn'))
+  // console.log('args', args)
   // const spy = spyOn(CreateMarketplaceForm, 'submittedForm')
 
   // await waitFor(() => expect(args).toHaveBeenCalled())
 }
 
 // More on args: https://storybook.js.org/docs/vue/writing-stories/args
-Base.args = {
+SuccessSubmitted.args = {
+  data: 'test'
   // primary: true,
   // label: 'Button'
 }
+
+// SuccessSubmitted.data({
+//   data: 'test'
+// })
