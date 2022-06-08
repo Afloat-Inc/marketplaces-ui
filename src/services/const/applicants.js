a17a9da702a37dbda4c23120e159236db702e70a
Jose <josemaria@hashed.io>
2022-06-08 12:40:48 -0500

    Implemented new afloat user flow and clean project

---
+++ src/services/const/applicants.js
@@ -0,0 +1,60 @@
+const applicants = [
+  {
+    id: '0',
+    address: '5DaWmLfzBTLbKFwBC5YxtAQ45XMSAQCDLcZL6zW9ZiJsGSST',
+    notes: 'This is just a test',
+    files: [
+      {
+        displayName: 'CV',
+        cid: 'thisisacid'
+      },
+      {
+        displayName: 'SAT',
+        cid: 'thisisacid'
+      },
+      {
+        displayName: 'Custom',
+        cid: 'thisisacid'
+      }
+    ]
+  },
+  {
+    id: '1',
+    address: '5HGZfBpqUUqGY7uRCYA6aRwnRHJVhrikn8to31GcfNcifkym',
+    notes: 'I want to apply to this marketplace',
+    files: [
+      {
+        displayName: 'Profile',
+        cid: 'thisisacid'
+      },
+      {
+        displayName: 'Custom',
+        cid: 'thisisacid'
+      },
+      {
+        displayName: 'Custom',
+        cid: 'thisisacid'
+      }
+    ]
+  },
+  {
+    id: '3',
+    address: '5CmFmVadzNQFaeiyXXNugRXT1MuaoocUyogtYHEQeWjGp7pX',
+    notes: 'I just want to see the marketplace',
+    files: [
+      {
+        displayName: 'Custom',
+        cid: 'thisisacid'
+      },
+      {
+        displayName: 'Picture',
+        cid: 'thisisacid'
+      },
+      {
+        displayName: 'SAT',
+        cid: 'thisisacid'
+      }
+    ]
+  }
+]
+export default applicants
