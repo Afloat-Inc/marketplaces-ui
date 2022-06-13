3461e9c0b954d0ab24bebf338009d81d4f86436e
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-13 16:50:18 -0500

    Modified the way to load the image to solve a storybook issue

--- src/components/common/logo.vue
+++ src/components/common/logo.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .row
-  q-img(src="~/assets/afloat-logo.svg" class="logo-icon")
+  q-img(:src="require('~/assets/afloat-logo.svg')" class="logo-icon")
   span(v-if="includeSubtitle" class="subtitle")
     a The TAXGIVING&reg; Company
 </template>
@@ -24,15 +24,16 @@ export default {
   margin-left: 10px
   margin-right: 0px
 .subtitle
-  font-family: Whitney SSm A, Whitney SSm B
+  font-family: 'Afloat font', sans-serif
   font-style: normal
   font-weight: 600
-  font-size: 1rem
-  margin-top: 2rem
+  font-size: 1.1rem
+  margin-top: 1.6rem
   margin-left: 10px
   margin-right: 10px
 a
-  color: $primary
+  color: #2EB7E5 !important
 a:hover
-  color: #007bff
+  color: #0056b3 !important
+  text-decoration: none
 </style>
