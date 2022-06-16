d182d506fa199270cf0ef6620bd1201dcac59dc4
AlejandroM1ghty <alejandro@m1ghty.io>
2022-06-16 11:58:19 -0500

    Modified the look & feel of the copyrigth according the new Font added

--- src/components/footer/components/copyright.vue
+++ src/components/footer/components/copyright.vue
@@ -13,14 +13,17 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~/css/app.styl'
 .sub
   vertical-align: sub
   font-size: 10px
 .copyright
-  position: relative
-  opacity: 0.4
-  font-family: Whitney SSm A,Whitney SSm B
+  position: relative !important
+  opacity: 0.2 !important
+  font-family: 'Whitney SSm A', 'Whitney SSm B', sans-serif !important
   font-style: italic
   font-size: 0.9rem
-  color: $grey
+  color: $color-neutral-grey-5
+  letter-spacing: 0.875px !important
+  font-weight: $font-weight-semibold
 </style>
